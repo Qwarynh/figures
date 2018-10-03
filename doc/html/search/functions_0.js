e78ae3966111432cdf4b9f577e575b718c0d626c
Euphoria <eupho.42@gmail.com>
2018-10-03 12:16:53 +0200

    MàJ doc Doxygen

--- doc/html/search/functions_0.js
+++ doc/html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['perimetre',['perimetre',['../classcercle.html#a1ffef36ca8e16782d03e44a91639c47a',1,'cercle::perimetre()'],['../classfigure.html#ad6f996a759902c54d77af60047f54e4f',1,'figure::perimetre()'],['../classrectangle.html#abf7908ec249774c1d68c5285b68271f8',1,'rectangle::perimetre()'],['../classtriangle.html#a079d90208145fc5cb3f016b42b0ae69d',1,'triangle::perimetre()']]]
+  ['perimetre',['perimetre',['../classcercle.html#a55f5bfa84171ffc0c01bd75df6cbf75e',1,'cercle::perimetre()'],['../classfigure.html#aa17af226caeac91d8a3f67276bcbd2b6',1,'figure::perimetre()'],['../classrectangle.html#a5720e82bd36ccfe8f17770523e3f033d',1,'rectangle::perimetre()'],['../classtriangle.html#a05b279c1d87e79ce6a724a797406fd42',1,'triangle::perimetre()']]]
 ];
--- doc/html/search/functions_1.js
+++ doc/html/search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['surface',['surface',['../classcercle.html#abb01ec58845bd1340c2f614c5194bcb5',1,'cercle::surface()'],['../classfigure.html#ad85963e43100c730015701b366755dda',1,'figure::surface()'],['../classrectangle.html#a9996c8fd7c082a3cdcea7f6946348794',1,'rectangle::surface()'],['../classtriangle.html#ad901747ecf12bd9306efe6b96edb7283',1,'triangle::surface()']]]
+  ['surface',['surface',['../classcercle.html#a140f47009fdc3f4ea8c93641538a3778',1,'cercle::surface()'],['../classfigure.html#ae301cacba1033c35834008e63ef103e8',1,'figure::surface()'],['../classrectangle.html#a7b881b0262d8e4ea913d188989399448',1,'rectangle::surface()'],['../classtriangle.html#a21d60b3b0972ed273858f81c4bf0b616',1,'triangle::surface()']]]
 ];
